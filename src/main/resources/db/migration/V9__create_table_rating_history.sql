9502014d55380b133c0c539653abbfb4d7390981
Jake <zubarev.e.ev@gmail.com>
2022-06-05 00:55:46 +0300

    changed rating_info

--- src/main/resources/db/migration/V9__create_table_rating_history.sql
+++ src/main/resources/db/migration/V9__create_table_rating_history.sql
@@ -1,14 +1,9 @@
 create table if not exists rating_history
 (
     id        bigint generated always as identity primary key,
-    event_id  bigint  not null,
     game_id   bigint  not null,
     player_id bigint  not null,
     change    integer not null,
-
-    CONSTRAINT fk_exists_event
-        FOREIGN KEY (event_id)
-            REFERENCES event (id),
 
     CONSTRAINT fk_exists_game
         FOREIGN KEY (game_id)
